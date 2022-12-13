bc86534e08de6aafe7603a6c92c69bc1a208a6a9
obiomaokorowu <austinobi983@gmail.com>
2022-12-13 13:20:01 +0100

    initial commit

---
+++ terraform/provisioners.tf
@@ -0,0 +1,34 @@
+########## PLUGINS and TF VERSION ##########
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.30.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.3.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
+
+########## REGION OF DEPLOYMENT ##########
+provider "aws" {
+  region  = var.aws_region
+  profile = terraform-user
+  
+  default_tags {
+    tags = {
+      Environment = "Dev"
+      Project     = "Auto_tag"
+    }
+  }
+}
